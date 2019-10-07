c2b7ff46926b0dc71aec742800ca046db77904a1
ivKop <ivKop@users.noreply.github.com>
2019-10-07 22:39:22 +0300

    1 день

--- basic/first/01-intro.py
+++ basic/first/01-intro.py
@@ -6,3 +6,34 @@
 #  Примеры базового синтаксиса и работа с типами данных
 #  Числа, строки, списки, булево значение
 #
+# коментарий Ctrl + /
+simple = 10
+result = simple + 10
+
+print(1 / 2)
+print(1 // 2)
+print(1 % 2)
+
+print(type(simple))
+
+s1 = int(0.8)
+s2 = 10.0
+print(type(s1))
+print(type(s2))
+name = "fJoan"
+print(name)
+print(name + ' ' + name)
+print(name * 3)
+print("-"*30)
+name = name.capitalize()
+print(name)
+name = name.upper()
+print(name)
+
+
+age = int(input("Введите ваш возраст: "))
+print(age)
+if age > 10:
+    print("прівет")
+else:
+    print("Пока")
